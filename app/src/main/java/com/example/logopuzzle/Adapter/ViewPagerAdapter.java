94c2dd2e2e9c6de56278bdb2623fdd3087d86271
C22 <bhavadipkhunt21@gmail.com>
2023-05-23 12:55:25 +0530

    qww

--- app/src/main/java/com/example/logopuzzle/Adapter/ViewPagerAdapter.java
+++ app/src/main/java/com/example/logopuzzle/Adapter/ViewPagerAdapter.java
@@ -39,8 +39,7 @@ public class ViewPagerAdapter  extends RecyclerView.Adapter<ViewPagerAdapter.Use
     int t=0;
     ViewPager2 viewPager;
     private int cnt=1;
-    private int counter=1;
-    private int counter1=1;
+
 
     public ViewPagerAdapter(Context context, ArrayList<String> image, String level, ViewPager2 viewPager, int position) {
         this.context=context;
@@ -55,14 +54,20 @@ public class ViewPagerAdapter  extends RecyclerView.Adapter<ViewPagerAdapter.Use
     public ViewPagerAdapter.UserHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(context).inflate(R.layout.view_pager_item, parent, false);
         UserHolder userHolder=new UserHolder(view);
-        System.out.println("posi=="+position);
-        if (counter==1) {
-            t = 0;
-            userHolder.linearLayout.removeAllViews();
-            ans.delete(0,t);
-            inflateItem(userHolder, position);
-        }
-            System.out.println("onCreateViewHolder called="+(counter++));
+
+
+        inflateItem(position,userHolder);
+        Log.d("SSS111", "creat=: "+position);
+
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int pos) {
+                super.onPageSelected(pos);
+                t=0;
+                Log.d("SSS111", "pagechange=: "+pos);
+                inflateItem(pos,userHolder);
+            }
+        });
 
         return userHolder;
     }
@@ -70,29 +75,141 @@ public class ViewPagerAdapter  extends RecyclerView.Adapter<ViewPagerAdapter.Use
     @Override
     public void onBindViewHolder(@NonNull ViewPagerAdapter.UserHolder holder, int position)
     {
-        System.out.println("onBindViewHolder called="+(counter1++));
-        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+        inflateItem(position,holder);
+        Log.d("SSS111", "bind=: "+position);
+   }
+    private void inflateItem( int position,UserHolder holder) {
+        System.out.println("Fun calls=> "+(cnt++)+"\t Position of Page="+position);
+
+        InputStream stream=null;
+        try {
+
+            if (level.equals("Level 1")) {
+                stream=context.getAssets().open("level 1UN/"+image.get(position));
             }
-
-            @Override
-            public void onPageSelected(int position) {
-                super.onPageSelected(position);
-                t=0;
-                ans.delete(0,t);
-                holder.linearLayout.removeAllViews();
-                inflateItem(holder,position);
+            if (level.equals("Level 2")) {
+                stream=context.getAssets().open("level 2UN/"+image.get(position));
             }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-                super.onPageScrollStateChanged(state);
+            if (level.equals("Level 3")) {
+                stream=context.getAssets().open("level 3UN/"+image.get(position));
             }
-        });
+            if (level.equals("Level 4")) {
+                stream=context.getAssets().open("level 4UN/"+image.get(position));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Drawable drawable= Drawable.createFromStream(stream,null);
+        holder.imageView.setImageDrawable(drawable);
+
+        str = image.get(position).split("\\.");
+        ch = str[0].toCharArray();
+        String finalans=str[0];
+      Log.d("SSS111", "Image Loaded=: "+str[0]);
+        for (int i=0;i<ch.length;i++)
+        {
+            ansarr.add(ch[i]);
+        }
+        for (int i=ch.length;i<holder.btn.length;i++)
+        {
+            char c= (char) (new Random().nextInt(122-97)+97);
+            ansarr.add(c);
+        }
+        Collections.shuffle(ansarr);
+        Collections.shuffle(ansarr);
+        for (int i=0;i<holder.btn.length;i++)
+        {
+            holder.btn[i].setText(""+ansarr.get(i));
+            //System.out.println("c"+ansarr.get(i));
+
+        }
+
+        ans_button = new Button[str[0].length()];
+        t=0;
+        holder.linearLayout.removeAllViews();
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, 1);
+        layoutParams.setMargins(5, 5, 5, 5);
+
+        int finalpos=position;
+
+        for (int i = 0; i < finalans.length(); i++) {
+            ans_button[i] = new Button(context);
+
+            ans_button[i].setLayoutParams(layoutParams);
+            ans_button[i].setBackgroundColor(context.getResources().getColor(R.color.purple_200));
+            holder.linearLayout.addView(ans_button[i]);
+
+        }
 
 
+        for (int i = 0; i < holder.btn.length; i++) {
+            holder.btn[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    for (int j = 0; j < holder.btn.length; j++) {
+                        if (view.getId() == holder.btn[j].getId()) {
+                            if (t < ans_button.length) {
+                                ans_button[t].setText("" + holder.btn[j].getText().toString());
+                                remove.add(j);
+                                ans.append(holder.btn[j].getText().toString());
+                                t++;
+                                holder.btn[j].setVisibility(View.INVISIBLE);
+                                CheckWin(ans,finalans,finalpos);
+                                Log.d("SSS1", "Image Loaded=: "+finalans);
+                                Log.d("SSS1", "Image Loaded=: "+finalpos);
+                                Log.d("SSS1", "Image Loaded=: "+ans);
+                            }
+                        }
+                    }
+                }
+
+
+            });
+
+
+        }
+//        holder.delet.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                for (int i=0;i<finalans.length();i++)
+//                {
+//                    holder.btn[remove.get(i)].setVisibility(View.VISIBLE);
+//                    if (ans_button[i].getText()!="") {
+//                        holder.btn[remove.get(i)].setText(ans_button[i].getText());
+//                        ans_button[i].setText("");
+//
+//                    }
+//                }
+//                viewPager.setCurrentItem(finalpos);
+//
+//            }
+//        });
+
+    }
+
+    private void CheckWin(StringBuffer ans, String finalans, int finalpos) {
+        Log.d("SSS1", "Image Loaded1=: "+finalans);
+        Log.d("SSS1", "Image Loaded1=: "+finalpos);
+        Log.d("SSS1", "Image Loaded1=: "+ans);
+        if (finalans.equalsIgnoreCase(String.valueOf(ans)))
+        {
+
+            ans.delete(0,finalans.length());
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle("Right Answer");
+            builder.setPositiveButton("Next", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                    viewPager.setCurrentItem(finalpos+1,false);
+
+                }
+            });
+            builder.show();
+        }
 
     }
     @Override
@@ -118,125 +235,5 @@ public class ViewPagerAdapter  extends RecyclerView.Adapter<ViewPagerAdapter.Use
             }
         }
     }
-    private void inflateItem(UserHolder holder, int position) {
-        System.out.println("Fun calls=> "+(cnt++)+"\t Position of Page="+position);
-        InputStream stream=null;
-        try {
 
-            if (level.equals("Level 1")) {
-                stream=context.getAssets().open("level 1UN/"+image.get(position));
-            }
-            if (level.equals("Level 2")) {
-                stream=context.getAssets().open("level 2UN/"+image.get(position));
-            }
-            if (level.equals("Level 3")) {
-                stream=context.getAssets().open("level 3UN/"+image.get(position));
-            }
-            if (level.equals("Level 4")) {
-                stream=context.getAssets().open("level 4UN/"+image.get(position));
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println("co"+context.getApplicationContext());
-        Drawable drawable= Drawable.createFromStream(stream,null);
-        holder.imageView.setImageDrawable(drawable);
-
-        str = image.get(position).split("\\.");
-        ch = str[0].toCharArray();
-        String finalans=str[0];
-        Log.d("SSS", "Image Loaded=: "+str[0]);
-        for (int i=0;i<ch.length;i++)
-        {
-            ansarr.add(ch[i]);
-        }
-        for (int i=ch.length;i<holder.btn.length;i++)
-        {
-            char c= (char) (new Random().nextInt(122-97)+97);
-            ansarr.add(c);
-        }
-        Collections.shuffle(ansarr);
-        Collections.shuffle(ansarr);
-        for (int i=0;i<holder.btn.length;i++)
-        {
-            holder.btn[i].setText(""+ansarr.get(i));
-            //System.out.println("c"+ansarr.get(i));
-
-        }
-
-        int finalpos=position;
-        ans_button = new Button[str[0].length()];
-        for (int i = 0; i < ch.length; i++) {
-            ans_button[i] = new Button(context.getApplicationContext());
-            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, 1);
-            layoutParams.setMargins(5, 5, 5, 5);
-            ans_button[i].setLayoutParams(layoutParams);
-            ans_button[i].setBackgroundColor(context.getResources().getColor(R.color.purple_200));
-            holder.linearLayout.addView(ans_button[i]);
-            ansarr.clear();
-        }
-
-        for (int i = 0; i < holder.btn.length; i++) {
-            holder.btn[i].setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    for (int i = 0; i < holder.btn.length; i++) {
-                        if (view.getId() == holder.btn[i].getId()) {
-                            if (t < ch.length) {
-                                ans_button[t].setText("" + holder.btn[i].getText());
-                                remove.add(i);
-                                ans.append(holder.btn[i].getText());
-                                t++;
-                                holder.btn[i].setVisibility(View.INVISIBLE);
-                                CheckWin(ans);
-                            }
-                        }
-                    }
-                }
-
-                private void CheckWin(StringBuffer ans) {
-                    if (finalans.equalsIgnoreCase(String.valueOf(ans)))
-                    {
-
-                        ans.delete(0,finalans.length());
-
-                        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-                        builder.setTitle("Good");
-                        builder.setPositiveButton("Next", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                holder.linearLayout.removeAllViews();
-                                viewPager.setCurrentItem(finalpos+1);
-                            }
-                        });
-                        builder.show();
-                    }
-                    else
-                    {
-                        viewPager.setCurrentItem(finalpos);
-
-                    }
-                }
-            });
-
-        }
-        holder.delet.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                for (int i=0;i<finalans.length();i++)
-                {
-                    holder.btn[remove.get(i)].setVisibility(View.VISIBLE);
-                    if (ans_button[i].getText()!="") {
-                        holder.btn[remove.get(i)].setText(ans_button[i].getText());
-                        ans_button[i].setText("");
-
-                    }
-                }
-                viewPager.setCurrentItem(finalpos);
-                remove.clear();
-            }
-        });
-
-    }
 }
